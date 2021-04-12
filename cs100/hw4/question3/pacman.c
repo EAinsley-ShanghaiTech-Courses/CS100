a285666a3aa999811efc4323c88c07e531f1e980
EAinsley <vhtmscyo@gmail.com>
2021-04-12 22:50:33 +0800

    improve pacman

--- cs100/hw4/question3/pacman.c
+++ cs100/hw4/question3/pacman.c
@@ -2,18 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char *Board[7] = {"....#........#....", ".##.#.######.#.##.",
-                        ".#..............#.", ".#.....##.##.##.#.",
-                        "...........#......", ".#.##.######.##.#.",
-                        "....#...C....#...."
-
-};
-
-const int MAX_GHOST = 30;
 typedef enum gameState { losing, onGoing, winning } GameState;
 typedef enum direction { up, down, left, right, idle } Direction;
-const int DELTAC[4] = {[left] = -1, [right] = 1};
-const int DELTAR[4] = {[up] = -1, [down] = 1};
+typedef enum gridtype {
+  empty = ' ',
+  pacman = 'C',
+  ghost = '@',
+  food = '.',
+  wall = '#'
+} Gridtype;
 typedef struct ghost {
   int r, c;
   Direction direction;
@@ -30,78 +27,94 @@ typedef struct game {
   int score;
   GameState state;
 } Game;
+const int MAX_GHOST = 30;
+const int FOOD_SCORE = 10;
+const char *Board[7] = {"....#........#....", ".##.#.######.#.##.",
+                        ".#..............#.", ".#.....##.##.##.#.",
+                        "...........#......", ".#.##.######.##.#.",
+                        "....#...C....#...."
+
+};
+const Ghost Ghosts[3] = {{2, 4, right}, {3, 6, left}, {3, 5, up}};
 
 Game *NewGame(int rows, int columns);
+void EndGame(Game *game);
+Direction Getinput();
+void PrintGame(const Game *game);
+void IninitialGame(Game *game, const char *board[], const Ghost *ghosts,
+                   int rows, int columns, int ghostnum);
+
 bool AddWall(Game *game, int r, int c);
 bool AddFood(Game *game, int r, int c);
 bool AddPacman(Game *game, int r, int c);
-void MovePacman(Game *game, Direction direction);
-void EndGame(Game *game);
 bool AddGhost(Game *game, int r, int c, Direction direction);
+
+Direction ReverseDirection(Direction *direction);
+Gridtype TryMove(Game *game, Direction direction, int *r, int *c,
+                 Gridtype trail);
+void MovePacman(Game *game, Direction direction);
 void MoveGhosts(Game *game);
 
 int main() {
-  Game *game = NewGame(7, 18);
-  for (int i = 0; i < 7; i++)
-    for (int j = 0; j < 18; j++) {
-      switch (Board[i][j]) {
-      case '.':
-        AddFood(game, i, j);
-        break;
-      case '#':
-        AddWall(game, i, j);
-        break;
-      case 'C':
-        AddPacman(game, i, j);
-      default:
-        break;
-      }
+  char again;
+  do {
+    Game *game = NewGame(7, 18);
+    IninitialGame(game, Board, Ghosts, 7, 18, 3);
+    PrintGame(game);
+    while (game->state == onGoing) {
+      Direction usermoving = Getinput();
+      MovePacman(game, usermoving);
+      MoveGhosts(game);
+      PrintGame(game);
     }
-  AddGhost(game, 2, 4, right);
-  AddGhost(game, 3, 6, left);
-  AddGhost(game, 3, 5, up);
+    if (game->state == losing)
+      puts("lost!");
+    else
+      puts("win!");
+    puts("Try again? ('y' to restart)");
+    EndGame(game);
+    again = getchar();
+    getchar();
+  } while (again == 'y');
+  puts("Goodbye!");
+  return 0;
+}
+
+void PrintGame(const Game *game) {
+  system("clear");
   for (int i = 0; i < game->rows; i++) {
     for (int j = 0; j < game->columns; j++)
       putchar(game->grid[i][j]);
     putchar('\n');
   }
-  while (game->state == onGoing) {
-    char ch;
-    Direction di;
-    ch = getchar();
-    getchar();
-    switch (ch) {
-    case 'w':
-      di = up;
-      break;
-    case 's':
-      di = down;
-      break;
-    case 'a':
-      di = left;
-      break;
-    case 'd':
-      di = right;
-      break;
-    default:
-      di = idle;
-      break;
-    }
-    MovePacman(game, di);
-    MoveGhosts(game);
-    system("clear");
-    for (int i = 0; i < game->rows; i++) {
-      for (int j = 0; j < game->columns; j++)
-        putchar(game->grid[i][j]);
-      putchar('\n');
-    }
+  puts("Use w/a/s/d to move, others to stay");
+  printf("Your score: %d, food left: %d\n", game->score, game->foodCount);
+}
+
+Direction Getinput() {
+  char ch;
+  Direction di;
+  ch = getchar();
+  switch (ch) {
+  case 'w':
+    di = up;
+    break;
+  case 's':
+    di = down;
+    break;
+  case 'a':
+    di = left;
+    break;
+  case 'd':
+    di = right;
+    break;
+  default:
+    di = idle;
+    break;
   }
-  if (game->state == losing)
-    puts("lost!");
-  else
-    puts("win!");
-  EndGame(game);
-  return 0;
+  while ((getchar()) != '\n')
+    continue;
+  return di;
 }
 
 Game *NewGame(int rows, int columns) {
@@ -110,7 +123,7 @@ Game *NewGame(int rows, int columns) {
   for (int i = 0; i < rows; i++) {
     gridrows[i] = (char *)malloc(sizeof(char) * columns);
     for (int j = 0; j < columns; j++)
-      gridrows[i][j] = ' ';
+      gridrows[i][j] = empty;
   }
   newgame->grid = gridrows;
   newgame->columns = columns;
@@ -121,6 +134,27 @@ Game *NewGame(int rows, int columns) {
   newgame->headghost = newgame->tailghost = NULL;
   newgame->ghostnum = 0;
   return newgame;
+}
+
+void IninitialGame(Game *game, const char *board[], const Ghost *ghosts,
+                   int rows, int columns, int ghostnum) {
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < columns; j++)
+      switch (board[i][j]) {
+      case food:
+        AddFood(game, i, j);
+        break;
+      case wall:
+        AddWall(game, i, j);
+        break;
+      case pacman:
+        AddPacman(game, i, j);
+      default:
+        break;
+      }
+  for (int i = 0; i < ghostnum; i++) {
+    AddGhost(game, ghosts[i].r, ghosts[i].c, ghosts[i].direction);
+  }
 }
 
 void EndGame(Game *game) {
@@ -140,8 +174,8 @@ void EndGame(Game *game) {
 
 bool AddWall(Game *game, int r, int c) {
   if (r >= 0 && r < game->rows && c >= 0 && c < game->columns &&
-      game->grid[r][c] == ' ') {
-    game->grid[r][c] = '#';
+      game->grid[r][c] == empty) {
+    game->grid[r][c] = wall;
     return true;
   }
   return false;
@@ -149,9 +183,9 @@ bool AddWall(Game *game, int r, int c) {
 
 bool AddFood(Game *game, int r, int c) {
   if (r >= 0 && r < game->rows && c >= 0 && c < game->columns &&
-      game->grid[r][c] == ' ') {
+      game->grid[r][c] == empty) {
     game->foodCount++;
-    game->grid[r][c] = '.';
+    game->grid[r][c] = food;
     return true;
   }
   return false;
@@ -159,8 +193,8 @@ bool AddFood(Game *game, int r, int c) {
 
 bool AddPacman(Game *game, int r, int c) {
   if (game->pacmanr == -1 && game->pacmanc == -1 && r >= 0 && r < game->rows &&
-      c >= 0 && c < game->columns && game->grid[r][c] == ' ') {
-    game->grid[r][c] = 'C';
+      c >= 0 && c < game->columns && game->grid[r][c] == empty) {
+    game->grid[r][c] = pacman;
     game->pacmanr = r;
     game->pacmanc = c;
     return true;
@@ -171,26 +205,17 @@ bool AddPacman(Game *game, int r, int c) {
 void MovePacman(Game *game, Direction direction) {
   if (game->pacmanr == -1 && game->pacmanc == -1)
     return;
-  int pacmanr = game->pacmanr + DELTAR[direction];
-  int pacmanc = game->pacmanc + DELTAC[direction];
   game->score--;
-  if (pacmanr < game->rows && pacmanr >= 0 && pacmanc < game->columns &&
-      pacmanc >= 0 && game->grid[pacmanr][pacmanc] != '#') {
-    game->grid[game->pacmanr][game->pacmanc] = ' ';
-    if (game->grid[pacmanr][pacmanc] != '@') {
-      if (game->grid[pacmanr][pacmanc] == '.') {
-        game->score += 10;
-        game->foodCount--;
-        if (!game->foodCount)
-          game->state = winning;
-      }
-      game->grid[pacmanr][pacmanc] = 'C';
-      game->pacmanr = pacmanr;
-      game->pacmanc = pacmanc;
-    } else {
-      game->state = losing;
-      game->pacmanr = game->pacmanc = -1;
-    }
+  Gridtype nextgrid =
+      TryMove(game, direction, &game->pacmanr, &game->pacmanc, empty);
+  if (nextgrid == ghost) {
+    game->state = losing;
+    game->pacmanc = game->pacmanr = -1;
+  } else if (nextgrid == food) {
+    game->score += FOOD_SCORE;
+    game->foodCount--;
+    if (!game->foodCount)
+      game->state = winning;
   }
 }
 
@@ -199,15 +224,15 @@ bool AddGhost(Game *game, int r, int c, Direction direction) {
     return false;
   if (r < 0 || r >= game->rows || c < 0 || c >= game->columns)
     return false;
-  if (game->grid[r][c] != '.' && game->grid[r][c] != ' ')
+  if (game->grid[r][c] != food && game->grid[r][c] != empty)
     return false;
   Ghost *newghost = (Ghost *)malloc(sizeof(Ghost));
   newghost->r = r;
   newghost->c = c;
   newghost->direction = direction;
   newghost->next = NULL;
-  newghost->possesfood = (game->grid[r][c] == '.');
-  game->grid[r][c] = '@';
+  newghost->possesfood = (game->grid[r][c] == food);
+  game->grid[r][c] = ghost;
   if (!game->headghost)
     game->headghost = game->tailghost = newghost;
   else {
@@ -220,61 +245,78 @@ bool AddGhost(Game *game, int r, int c, Direction direction) {
 
 void MoveGhosts(Game *game) {
   Ghost *currentghost = game->headghost;
-  bool movedtwice = false;
   while (currentghost) {
-    Direction direction = currentghost->direction;
-    int ghostr = currentghost->r + DELTAR[direction];
-    int ghostc = currentghost->c + DELTAC[direction];
-    if (ghostr >= 0 && ghostr < game->rows && ghostc >= 0 &&
-        ghostc < game->columns && game->grid[ghostr][ghostc] != '#' &&
-        game->grid[ghostr][ghostc] != '@') {
-      // recover food.
-      if (currentghost->possesfood)
-        game->grid[currentghost->r][currentghost->c] = '.';
-      else
-        game->grid[currentghost->r][currentghost->c] = ' ';
-      switch (game->grid[ghostr][ghostc]) {
-      case 'C':
-        game->state = losing;
-        break;
-      case ' ':
-        currentghost->possesfood = false;
-        break;
-      case '.':
-        currentghost->possesfood = true;
-        break;
-      default:
-        break;
-      }
-      // move ghost
-      game->grid[ghostr][ghostc] = '@';
-      currentghost->r = ghostr;
-      currentghost->c = ghostc;
-    } else {
-      Direction prederection = currentghost->direction;
-      switch (prederection) {
-      case up:
-        currentghost->direction = down;
-        break;
-      case down:
-        currentghost->direction = up;
-        break;
-      case left:
-        currentghost->direction = right;
-        break;
-      case right:
-        currentghost->direction = left;
-        break;
-      default:
-        break;
-      }
-      // try again
-      if (!movedtwice) {
-        movedtwice = true;
-        continue;
-      }
+    Gridtype trail = currentghost->possesfood ? food : empty;
+    Gridtype nextgrid = TryMove(game, currentghost->direction, &currentghost->r,
+                                &currentghost->c, trail);
+    if (nextgrid == ghost || nextgrid == wall) {
+      nextgrid = TryMove(game, ReverseDirection(&currentghost->direction),
+                         &currentghost->r, &currentghost->c, trail);
+    }
+    switch (nextgrid) {
+    case pacman:
+      game->state = losing;
+      break;
+    case empty:
+      currentghost->possesfood = false;
+      break;
+    case food:
+      currentghost->possesfood = true;
+      break;
+    case wall:
+    case ghost:
+      currentghost->direction = ReverseDirection(&currentghost->direction);
+      break;
+    default:
+      break;
     }
     currentghost = currentghost->next;
-    movedtwice = false;
   }
+}
+
+Gridtype TryMove(Game *game, Direction direction, int *r, int *c,
+                 Gridtype trail) {
+  if (direction == idle)
+    return game->grid[*r][*c];
+  const int DELTAC[5] = {[left] = -1, [right] = 1};
+  const int DELTAR[5] = {[up] = -1, [down] = 1};
+  int aimr = *r + DELTAR[direction], aimc = *c + DELTAC[direction];
+  Gridtype movetype = game->grid[*r][*c];
+  if (aimr >= 0 && aimc >= 0 && aimr < game->rows && aimc < game->columns) {
+    Gridtype aimtype = game->grid[aimr][aimc];
+    if (aimtype != wall && aimtype != ghost) {
+      game->grid[aimr][aimc] = game->grid[*r][*c];
+      game->grid[*r][*c] = trail;
+      *r = aimr;
+      *c = aimc;
+    } else if (aimtype == ghost && movetype == pacman) {
+      game->grid[*r][*c] = trail;
+      *r = *c = -1;
+    }
+    return aimtype;
+  }
+  return wall;
+}
+
+Direction ReverseDirection(Direction *direction) {
+  if (*direction == idle)
+    return idle;
+  Direction predirection = *direction;
+  switch (predirection) {
+  case up:
+    *direction = down;
+    break;
+  case down:
+    *direction = up;
+    break;
+  case left:
+    *direction = right;
+    break;
+  case right:
+    *direction = left;
+    break;
+  default:
+    break;
+  }
+  return *direction;
 }
